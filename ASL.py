19b090450475ec17170d1d185780ba6c2e94fe44
ayaank37 <133443388+ayaank37@users.noreply.github.com>
2023-05-13 15:01:56 -0400

    Add files via upload

---
+++ ASL.py
@@ -0,0 +1,91 @@
+from PIL import Image
+dude= int(input('how many letters in sentence/word(num)'))
+for num in range(dude):
+    lol = input('type a letter(lower case)')
+    if lol == 'a':
+        image = Image.open('A-SignLanguage-01-1080x1080.jpg')
+        image.show()
+    elif lol == 'b':
+        image = Image.open('B.npg')
+        image.show()
+    elif lol == 'c':
+        image = Image.open('c.jpg')
+        image.show()
+    elif lol == 'd':
+        image = Image.open('d.jpg')
+        image.show()
+    elif lol == 'e':
+        image = Image.open('e.jpg')
+        image.show()
+    elif lol == 'f':
+        image = Image.open('f.jpg')
+        image.show()
+    elif lol == 'g':
+        image = Image.open('g.jpg')
+        image.show()
+    elif lol == 'h':
+        image = Image.open('h.jpg')
+        image.show()
+    elif lol == 'i':
+        image = Image.open('i2.jpg')
+        image.show()
+    elif lol == 'j':
+        image = Image.open('j.jpg')
+        image.show()
+    elif lol == 'k':
+        image = Image.open('k.jpg')
+        image.show()
+    elif lol == 'l':
+        image = Image.open('l.jpg')
+        image.show()
+    elif lol == 'm':
+        image = Image.open('m.jpg')
+        image.show()
+    elif lol == 'n':
+        image = Image.open('n.jpg')
+        image.show()
+    elif lol == 'o':
+        image = Image.open('o.jpg')
+        image.show()
+    elif lol == 'p':
+        image = Image.open('p.jpg')
+        image.show()
+    elif lol == 'q':
+        image = Image.open('q.jpg')
+        image.show()
+    elif lol == 'r':
+        image = Image.open('r.jpg')
+        image.show()
+    elif lol == 's':
+        image = Image.open('s.jpg')
+        image.show()
+    elif lol == 't':
+        image = Image.open('t.jpg')
+        image.show()
+        lol = input('type a letter(lower case)')
+    elif lol == 'u':
+        image = Image.open('u.jpg')
+        image.show()
+    elif lol == 'v':
+        image = Image.open('v.jpg')
+        image.show()
+    elif lol == 'w':
+        image = Image.open('w.jpg')
+        image.show()
+    elif lol == 'x':
+        image = Image.open('x.jpg')
+        image.show()
+    elif lol == 'y':
+        image = Image.open('y.jpg')
+        image.show()
+    elif lol == 'z':
+        image = Image.open('z.jpg')
+        image.show()
+    else:
+        print('make sure you are writing lower case!!')
+
+
+
+
+
+
